bd5431f0fdb25c318a100101b6b777c684a93821
mehul4804 <mehul.ghosh4804@gmail.com>
2016-11-14 05:03:26 +0530

    Add files via upload

---
+++ Add_1.java
@@ -0,0 +1,13 @@
+class Add_1
+{
+public static void main(String[] args)
+{
+int c;
+System.out.print("Enter the value of First number :");
+int First_number = Integer.parseInt(System.console().readLine());
+System.out.print("Enter the value of First number :");
+int Second_number = Integer.parseInt(System.console().readLine());
+c=First_number + Second_number;
+System.out.println("After Adding the value of First_number and Second_number :"+ c);
+}
+}
